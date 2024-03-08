421acdd6852c0062b9c53628989326ec36670497
AShakhmatov <Coluchy@yandex.ru>
2024-03-09 01:50:52 +0600

    new code

--- src/test/java/tests/PracticeFormRemoteTestsWithJenkins.java
+++ src/test/java/tests/PracticeFormRemoteTestsWithJenkins.java
@@ -26,7 +26,7 @@ public class PracticeFormRemoteTestsWithJenkins {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://Coluchy:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
