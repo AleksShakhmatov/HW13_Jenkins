cfcd7d05946ea8d9fc9e09689e935455ba7e8618
AShakhmatov <Coluchy@yandex.ru>
2024-03-09 03:07:32 +0600

    new code

--- src/test/java/tests/PracticeFormRemoteTests.java
+++ src/test/java/tests/PracticeFormRemoteTests.java
@@ -24,7 +24,7 @@ public class PracticeFormRemoteTests {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://Coluchy:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
